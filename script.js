597598f91e6c6c36f9224a016454b557aa8a7764
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-22 17:45:36 +0200

    ADD: style

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const onOffbutton = document.getElementById('btn');
 const lampImg = document.getElementById('lampimg');
-
+// Lo stato della mia lampadina
 let lampOn = false;
+const lampImgON = "./img/yellow_lamp.png";
+const lampImgOFF = "./img/white_lamp.png";
+const btnTextON = 'Accendi';
+const btnTextOFF = 'Spegni';
+
+lampOn ? lampImg.src = lampImgON : lampImg.src = lampImgOFF
+lampOn ? onOffbutton.innerText = btnTextOFF : onOffbutton.innerText = btnTextON
+
 
 onOffbutton.addEventListener('click', function () {
     
-
+    
     if (lampOn === false) {
-        onOffbutton.innerText = 'Spegni';
-        onOffbutton.classList.remove('btn-warning');
-        onOffbutton.classList.add('btn-danger');
-        lampImg.src = "./img/yellow_lamp.png";
         lampOn = true;
+        onOffbutton.innerText = btnTextOFF;
+        onOffbutton.classList.remove('btn-dark');
+        lampImg.classList.add('mirror');
+        onOffbutton.classList.add('btn-light');
+        lampImg.src = lampImgON;
+        
 
     } else if (lampOn === true) {
-        onOffbutton.innerText = 'Accendi';
-        lampImg.src = "./img/white_lamp.png";
-        onOffbutton.classList.remove('btn-danger');
-        onOffbutton.classList.add('btn-warning');
         lampOn = false;
+        onOffbutton.innerText = btnTextON;
+        lampImg.src = lampImgOFF;
+        onOffbutton.classList.remove('btn-light');
+        onOffbutton.classList.add('btn-dark');
+        lampImg.classList.remove('mirror');
+        
     }
 
 
