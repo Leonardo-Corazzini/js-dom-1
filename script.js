d8c465424e2465dc3f2fe6363a86f957c523fea7
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-22 15:23:55 +0200

    ADD: bonus

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ const lampImg = document.getElementById('lampimg');
 console.log(lampImg)
 
 onOffbutton.addEventListener('click',function(){
-    lampImg.src = "./img/yellow_lamp.png"
-})
+    lampImg.classList.toggle('on');
+
+    if (lampImg.classList.contains('on')) {
+      onOffbutton.innerText = 'Spengi';
+      lampImg.src = "./img/yellow_lamp.png"
+    } else {
+      onOffbutton.innerText = 'Accendi';
+      lampImg.src = "./img/white_lamp.png"
+    }
+
+   
+}
+);
+    
